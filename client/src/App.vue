2eb6e7439ec81a1f69c1da06ce2af33f391eac8c
filippo-ceradini <90599585+filippo-ceradini@users.noreply.github.com>
2023-04-26 14:17:15 +0200

    Vue Project

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,6 +7,13 @@
 </template>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50
+}
 
 nav {
   padding: 30px;
@@ -16,4 +23,9 @@ nav a {
   font-weight: bold;
   color: #2c3e50;
 }
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
 </style>
